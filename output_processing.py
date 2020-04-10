0f2ff0280ebaf88c00b90c39cd722450fb96c788
Raksha P Rao <raksha.rao2009@gmail.com>
2020-04-10 12:04:28 +0100

    Added f1 score for counting num of cars

--- output_processing.py
+++ output_processing.py
@@ -8,6 +8,7 @@ import pickle
 import csv
 import os
 from sklearn.metrics import f1_score
+from collections import defaultdict
 
 classes = ["hatchback", "sedan"]
 colors = ["black", "silver", "red", "white", "blue"]
@@ -15,16 +16,27 @@ colors = ["black", "silver", "red", "white", "blue"]
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
 class ProcessOutput:
+    def process_pkl_count(self):
+        predictions_dict = pickle.load(open(current_dir + "\predictions.pkl", "rb"))
+        frame_count_map = defaultdict(int)
+        for key in predictions_dict:
+            frame_num = key[7: -4]
+            frame_count_map[frame_num] += 1
+        self.frame_count_map = frame_count_map
+        
+        
     def process_predictions_pkl(self):
         predictions_dict = pickle.load(open(current_dir + "\predictions.pkl", "rb"))
-        print(predictions_dict)
+        pred_frame_count_map = self.frame_count_map
         tiny_yolo_pkl = pickle.load(open(current_dir + "\TinyYolo.pkl", "rb"))
         no_of_frames_not_in_pred = 0
-        y_true = []
-        y_predicted = []
+        y_count_true = []
+        y_count_predicted = []
         gt_predictions_dict = {}
         sedan_count = 0
         hatchback_count = 0
+        actual_num_cars = 0
+        predicted_num_cars = 0
 
         with open(current_dir + "\Ground_Truth.csv") as gt_csv:
             reader = csv.reader(gt_csv, delimiter=' ', quotechar='/')
@@ -34,27 +46,14 @@ class ProcessOutput:
                 line = row[0].split(',')
                 frame_num = line[0]
                 total_cars = int(line[11])
-                if (total_cars > 0):
-                    car_indexes = [ i for i, x in enumerate(line[1: -1]) if x == '1']
-                    car_indexes = car_indexes[:1]
-                    for index in car_indexes:
-                        is_hatchback = index >= 5
-                        is_sedan = index < 5
-                        if is_sedan:
-                            sedan_count += 1
-                        if is_hatchback:
-                            hatchback_count += 1
-                        actual_class = 0 if is_hatchback else 1
-                        try:
-                            predicted_class = predictions_dict[frame_num]
-                            y_true.append(actual_class)
-                            y_predicted.append(predicted_class)
-                        except Exception:
-                            no_of_frames_not_in_pred += 1
-        
-        print(no_of_frames_not_in_pred)
-        print(f1_score(y_true, y_predicted, average="weighted"))
+                y_count_true.append(total_cars)
+                try:
+                    y_count_predicted.append(pred_frame_count_map[frame_num])
+                except Exception:
+                    y_count_predicted.append(0)
+        print("The F1 score for Q1 is", f1_score(y_count_true, y_count_predicted, average="weighted"))
 
 if __name__ == "__main__":
     p = ProcessOutput()
+    p.process_pkl_count()
     p.process_predictions_pkl()
